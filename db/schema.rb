2f0a68b783dab9c5f2c5c6bfd32d376142c5f070
Maddison Murphy <maddison@puppyteeth.org>
2020-11-15 15:33:55 -0500

    scaffold

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2020_11_15_202334) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
